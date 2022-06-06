275880b1151c902e4fe510ad13be0836dbd65984
onurasln55 <onurasln55@gmail.com>
2022-06-06 18:05:18 +0300

    Update Virustotal_URL.py

--- Virustotal_URL.py
+++ Virustotal_URL.py
@@ -1,51 +1,39 @@
-import time
-
-import requests
-import json
-
+from requests import request,get
+from json import loads
 def vt_url_scan(url,api):
     vt = "https://www.virustotal.com/api/v3/urls"
     payload = "url="+url
-    headers = {
-        "Accept": "application/json",
-        "x-apikey": api,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
+    headers = {"Accept": "application/json","x-apikey": api,"Content-Type": "application/x-www-form-urlencoded"}
     try:
-        response = requests.request("POST", vt, data=payload, headers=headers)
-        json_response = json.loads(response.text)
+        response = request("POST", vt, data=payload, headers=headers)
+        json_response = loads(response.text)
         id=json_response["data"]["id"]
         id=id.split("-")
         id=id[1]
         url = "https://www.virustotal.com/api/v3/urls/" + id
-
-        headers = {
-            "Accept": "application/json",
-            "x-apikey": api
-        }
-        response = requests.get(url, headers=headers)
-        json_response = json.loads(response.text)
+        headers = {"Accept": "application/json","x-apikey": api}
+        response = get(url, headers=headers)
+        json_response = loads(response.text)
         malicious = json_response["data"]["attributes"]["last_analysis_stats"]["malicious"]
-
         return malicious
     except:
         print("vt_url_upload_hata")
         return '','',''
-
-def dosyadan_islem(api):
+def dosyadan_islem(api): #dosya konumu vermeden dosya arayüzü ile ekleme.
     from tkinter import Tk
     from tkinter.filedialog import askopenfilename
-
     pencere = Tk()
     pencere.attributes('-topmost', 1)
     pencere.withdraw()
-    x = askopenfilename(filetypes=[('.txt', '.txt')], title='URL listesi girin')
-
-    with open(x, 'r') as urls:
-        data=urls.readlines()
-        for i in data:
-            u=i.strip()
-            sonuc=vt_url_scan(i, api)
-            print(u+","+str(sonuc))
-
-dosyadan_islem("")#buraya api key ekleyin
+    x = askopenfilename(filetypes=[('.txt', '.txt')], title='URL listesi seçin')
+    try:
+        with open(x, 'r') as urls:
+            data=urls.readlines()
+            for i in data:
+                u=i.strip()
+                sonuc=vt_url_scan(i, api)
+                print(u+","+str(sonuc))
+    except:
+        print("File read error")
+api=input("Enter the Virustotal api key: ")#Bu bölgede api keyi dışarıdan giriyoruz.
+dosyadan_islem(api)
