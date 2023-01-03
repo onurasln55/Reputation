ca62ac62977255190d8bb721573bd43465204629
onurasln55 <onurasln55@gmail.com>
2023-01-03 10:32:19 +0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,5 +1,6 @@
 import requests
 import json
+
 def vt_hash_reputation(hash,api):
     url = "https://www.virustotal.com/api/v3/search?query="+hash
     headers = {
@@ -8,20 +9,20 @@ def vt_hash_reputation(hash,api):
     }
     try:
         response = requests.request("GET", url, headers=headers)
-        json_respone = json.loads(response.text)
-        r=json_respone["data"]
-        malicious=r[0]["attributes"]["last_analysis_stats"]["malicious"]
-        scan_date=r[0]["attributes"]["last_analysis_date"]
-        md5=r[0]["attributes"]["md5"]
-        scan_id=r[0]["id"]
+        json_response = json.loads(response.text)
+        r = json_response["data"]
+        malicious = r[0]["attributes"]["last_analysis_stats"]["malicious"]
+        scan_date = r[0]["attributes"]["last_analysis_date"]
+        md5 = r[0]["attributes"]["md5"]
+        scan_id = r[0]["id"]
         from datetime import datetime
-        scan_date=datetime.utcfromtimestamp(scan_date).strftime('%H:%M:%S %d-%m-%Y')
-        scan_link="https://www.virustotal.com/gui/file/"+scan_id
-        return malicious,scan_date,md5,scan_link
+        scan_date = datetime.utcfromtimestamp(scan_date).strftime('%H:%M:%S %d-%m-%Y')
+        scan_link = "https://www.virustotal.com/gui/file/" + scan_id
+        return malicious, scan_date, md5, scan_link
     except:
-        print("hash")
-        exit()
-def dosyadan_islem(api): #dosya konumu vermeden dosya arayüzü ile ekleme.
+        print("Hata oluştu!")
+
+def dosyadan_islem(api):
     from tkinter import Tk
     from tkinter.filedialog import askopenfilename
     pencere = Tk()
@@ -29,18 +30,18 @@ def dosyadan_islem(api): #dosya konumu vermeden dosya arayüzü ile ekleme.
     pencere.withdraw()
     x = askopenfilename(filetypes=[('.txt', '.txt')], title='URL listesi seçin')
     try:
-        with open(x, 'r') as urls:
-            data=urls.readlines()
+        with open(x, 'r', encoding='utf-8') as urls:
+            data = urls.readlines()
             for i in data:
-                u=i.strip()
+                u = i.strip()
                 try:
-                    sonuc=vt_hash_reputation(i, api)
+                    sonuc = vt_hash_reputation(u, api)
                 except:
                     pass
-                print(u+","+str(sonuc))
-                s=u+","+str(sonuc)+"\n"
+                print(u + ", " + str(sonuc))
+                s = u + ", " + str(sonuc) + "\n"
                 try:
-                    with open(x+'_s.txt', 'a') as f:
+                    with open(x + '_s.txt', 'a') as f:
                         f.write(s)
                         f.close()
                 except:
@@ -48,5 +49,4 @@ def dosyadan_islem(api): #dosya konumu vermeden dosya arayüzü ile ekleme.
     except:
         print("File read error")
 
-
-dosyadan_islem("")#api key yazımadan çalışmaz
+dosyadan_islem("")#api input
