2293bf06191c9a10fc176e8945995cee122f5dfe
Muhammet Onur <onurasln55@gmail.com>
2023-07-15 14:01:45 +0300

    Update ip.py
    İyileştirildi, Gelen bilgiler değiştirildi ve güncellendi.  ip adresi hakkında daha fazla bilgi gelmekte.

--- ip.py
+++ ip.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pycountry
 
 def api_check():
     try:
@@ -23,8 +24,6 @@ def api_check():
             print("API Kaydedildi programı yeniden başlatın.")
             api.close()
             return vt_api,abuseip_api
-
-
 def vt_ip_sorgu(ip,key_vt):
     url = "https://www.virustotal.com/api/v3/ip_addresses/"+ip
     headers = {
@@ -37,7 +36,6 @@ def vt_ip_sorgu(ip,key_vt):
         return json_response["data"]["attributes"]["last_analysis_stats"]["malicious"]
     except:
         return "Hata. Girdi veya API key hatalı Kontrol edin"
-
 def abuse_ip_sorgu(ip,key_abuse):
     url = 'https://api.abuseipdb.com/api/v2/check'
     querystring = {
@@ -51,9 +49,26 @@ def abuse_ip_sorgu(ip,key_abuse):
     try:
         response = requests.request(method='GET', url=url, headers=headers, params=querystring)
         json_response = json.loads(response.text)
-        return json_response["data"]["abuseConfidenceScore"]
+        return json_response["data"]["abuseConfidenceScore"],json_response["data"]["countryCode"],json_response["data"]["usageType"],json_response["data"]["isp"]
+
     except:
         return "Hata. Girdi veya API key hatalı kontrol edin"
+def country(code):
+    try:
+        country = pycountry.countries.get(alpha_2=code.upper())
+        if country:
+            return country.name
+    except LookupError:
+        pass
+    return None
+
+def country_code_to_name_restcountries(code):
+    url = f"https://restcountries.com/v3/alpha/{code}"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        return data["name"]["common"]
+    return None
 
 def ip():
     api_keys=api_check()
@@ -81,26 +96,34 @@ def ip():
             if cikti_dosyasi!="":
                 cikti_dosyasi=cikti_dosyasi+".txt"
                 with open(cikti_dosyasi, 'w+') as sonuc_dosyası:
-                    sonuc_dosyası.write("IP Adresi,AbuseipDB skoru, Virustotal skoru,AbuseipDB Link,Virustotal Link,IPalyzer Link\n")
-                    print("IP Adresi,AbuseipDB skoru, Virustotal skoru,AbuseipDB Link,Virustotal Link,IPalyzer Link")
+                    sonuc_dosyası.write("IP Adresi,Kurum,Ülke,Kullanım Tipi,AbuseipDB skoru, Virustotal skoru,AbuseipDB Link,Virustotal Link,IPalyzer Link\n")
+                    print("IP Adresi,Kurum,Ülke,Kullanım Tipi,AbuseipDB skoru, Virustotal skoru,AbuseipDB Link,Virustotal Link,IPalyzer Link")
                     with open(girdi_dosyasi) as f:
                         data = [row for row in f]
                         for d in data:
                             try:
-                                response_abuse = abuse_ip_sorgu(d,key_abuse)
-                                response_abuse = str(response_abuse)
+                                #AbuseIpDB işlemleri
+                                abuse_all = abuse_ip_sorgu(d,key_abuse)
+                                abuse_skor = str(abuse_all[0])
+                                abuse_country = str(abuse_all[1])
+                                abuse_usageType = str(abuse_all[2])
+                                abuse_isp = str(abuse_all[3])
+
+                                #Virustotal işlemleri
                                 response_vt=vt_ip_sorgu(d,key_vt)
                                 response_vt=str(response_vt)
                                 ip = d
                                 ip = str(ip)
 
-                                if response_abuse == "Hata. Girdi veya API key hatalı kontrol edin":
+                                if abuse_all == "Hata. Girdi veya API key hatalı kontrol edin":
                                     sonuc_dosyası.writelines(ip.strip() + ", ip girdisi hatalı lütfen düzeltin.\n")
                                     print(ip.strip() + ", ip girdisi hatalı lütfen düzeltin.")
 
                                 else:
-                                    sonuc_dosyası.writelines(ip.strip() + "," + response_abuse + "," + response_vt + " ,https://www.abuseipdb.com/check/" + ip.strip() + " ,https://www.virustotal.com/gui/ip-address/" + ip.strip() +", https://www.ipalyzer.com/"+ip.strip()+ "\n")
-                                    print(ip.strip() + "," + response_abuse + "," + response_vt + " ,https://www.abuseipdb.com/check/" + ip.strip() + " ,https://www.virustotal.com/gui/ip-address/" + ip.strip()+", https://www.ipalyzer.com/"+ip.strip())
+                                    sonuc_dosyası.writelines(ip.strip() + "," + abuse_isp + "," + str(country(
+                                        abuse_country)) + "," + abuse_usageType + "," + abuse_skor + "," + response_vt + " ,https://www.abuseipdb.com/check/" + ip.strip() + " ,https://www.virustotal.com/gui/ip-address/" + ip.strip() + ", https://www.ipalyzer.com/" + ip.strip() + "\n")
+                                    print(ip.strip() + "," + abuse_isp + "," + str(country(
+                                        abuse_country)) + "," + abuse_usageType + "," + "," + abuse_skor + "," + response_vt + " ,https://www.abuseipdb.com/check/" + ip.strip() + " ,https://www.virustotal.com/gui/ip-address/" + ip.strip() + ", https://www.ipalyzer.com/" + ip.strip())
                             except:
                                 print("hata")
                 sonuc_dosyası.close()
@@ -111,9 +134,17 @@ def ip():
     elif tur_secimi == 2:
         ip=input("Ip adresini girin:")
         vt_skor=str(vt_ip_sorgu(ip, key_vt))
-        abuse_skor=str(abuse_ip_sorgu(ip, key_abuse))
+        abuse_all=abuse_ip_sorgu(ip, key_abuse)
+        abuse_skor=str(abuse_all[0])
+        abuse_country=str(abuse_all[1])
+        abuse_usageType=str(abuse_all[2])
+        abuse_isp=str(abuse_all[3])
+        print("Kurum:" + abuse_isp)
+        print("Ülke:" + str(country(abuse_country)))
+        print("Kullanım tipi:" + abuse_usageType)
         print("Virustotal:"+vt_skor)
         print("AbuseIpDB:"+abuse_skor)
+
         if vt_skor!="Hata. Girdi veya API key hatalı Kontrol edin" or abuse_skor!="Hata. Girdi veya API key hatalı kontrol edin":
 
             print("https://www.virustotal.com/gui/ip-address/"+ip)
